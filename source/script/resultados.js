21e09508de8fc842d4b9a71430a9b52cc129972d
Ricardo Rosendo <88691117+RicardoRosendo98@users.noreply.github.com>
2023-02-10 19:25:36 -0300

    commit inicial

---
+++ source/script/resultados.js
@@ -0,0 +1,19 @@
+const somar = require('./valores');
+const multi = require('./valores');
+const divid = require('./valores');
+const sub = require('./valores');
+const nome = require('./valores');
+
+
+
+console.log('Somar: ', somar(10, 20));
+
+console.log('Multiplicar: ', multi(5, 47));
+
+console.log('Dividir: ', divid(7, 47));
+
+console.log('Subtrair: ', sub(50, 37));
+
+console.log('Seu Nome', nome("Ricardo"));
+
+
---
+++ source/script/valores.js
@@ -0,0 +1,28 @@
+function somar(a, b) {
+    return a + b;
+}
+
+function multi(c, d) {
+    return c * d;
+}
+
+function divid(e, f) {
+    return e + f;
+}
+
+function sub(g, h) {
+return g + h;
+}
+ 
+function nome(pessoa) {
+    return pessoa;
+}
+
+
+module.exports = somar;
+module.exports = multi;
+module.exports = divid;
+module.exports = sub;
+module.exports = nome;
+
+
